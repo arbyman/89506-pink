c629ef036b95f0226bebfc1f72f4034432154d4e
Sergey Vanin <bardelorean@yandex.ru>
2016-12-01 17:45:23 +0300

    Исправления-2

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,6 @@ var menu = document.querySelector('.main-nav');
 var button = document.querySelector('.page-header__toggle-menu');
 
 button.addEventListener('click', menuToggle);
-  
 
 function menuToggle() {
   menu.classList.toggle('main-nav--opened');
