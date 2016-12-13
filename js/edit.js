785fe2c8f253ce178b85d2e7a71720c258346975
Sergey Vanin <bardelorean@yandex.ru>
2016-12-13 12:41:39 +0300

    адаптивность

---
+++ js/edit.js
@@ -0,0 +1,36 @@
+var crop = document.querySelector('.photo-edit__crop');
+var fill = document.querySelector('.photo-edit__fill');
+var contr = document.querySelector('.photo-edit__contrast');
+
+var rangeCrop = document.querySelector('.photo-edit__range--crop');
+var rangeFill = document.querySelector('.photo-edit__range--fill');
+var rangeContr = document.querySelector('.photo-edit__range--contrast');
+
+crop.addEventListener('click', cropToggle);
+fill.addEventListener('click', fillToggle);
+contr.addEventListener('click', contrToggle);
+
+function cropToggle() {
+  crop.classList.add('photo-edit__crop--active');
+  fill.classList.remove('photo-edit__fill--active');
+  contr.classList.remove('photo-edit__contrast--active');
+  rangeCrop.classList.toggle('photo-edit__range--show');
+  rangeFill.classList.remove('photo-edit__range--show');
+  rangeContr.classList.remove('photo-edit__range--show');
+};
+function fillToggle() {
+  crop.classList.remove('photo-edit__crop--active');
+  fill.classList.add('photo-edit__fill--active');
+  contr.classList.remove('photo-edit__contrast--active');
+  rangeCrop.classList.remove('photo-edit__range--show');
+  rangeFill.classList.toggle('photo-edit__range--show');
+  rangeContr.classList.remove('photo-edit__range--show');
+};
+function contrToggle() {
+  crop.classList.remove('photo-edit__crop--active');
+  fill.classList.remove('photo-edit__fill--active');
+  contr.classList.add('photo-edit__contrast--active');
+  rangeCrop.classList.remove('photo-edit__range--show');
+  rangeFill.classList.remove('photo-edit__range--show');
+  rangeContr.classList.toggle('photo-edit__range--show');
+};
